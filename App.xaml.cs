72363aabd0589ca0bcd768ab2b844cc97d8e6afe
Andy James <andrewj@ksharp.net>
2024-04-17 01:02:16 +0100

    Add Azure PR Mate initial

---
+++ App.xaml.cs
@@ -0,0 +1,146 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Serilog;
+using Serilog.Events;
+
+// ReSharper disable PossibleNullReferenceException
+
+namespace AzurePRMate {
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application, IDisposable {
+
+        protected ContextMenu IconContextMenu;
+
+        private readonly AzurePrService _azurePrService;
+        private readonly int _reminderFrequency;
+        private readonly int _fetchFrequency;
+
+        private Timer _timer;
+        private DateTime _lastReminderTime;
+        private TaskbarIcon _taskbarIcon;
+
+        public App() {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File("prMate.log", LogEventLevel.Information)
+                .CreateLogger();
+            var config = new ConfigurationBuilder().AddJsonFile("config.json").AddUserSecrets<App>().Build();
+            this._azurePrService = new AzurePrService(config);
+            this._reminderFrequency = config.GetValue("ReminderFrequencyInMinutes", 30);
+            this._fetchFrequency = config.GetValue("FetchFrequencyInSeconds", 180);
+        }
+
+        public void Dispose() {
+            this._taskbarIcon?.Dispose();
+            this._timer?.Dispose();
+        }
+
+        protected override void OnStartup(StartupEventArgs _) {
+            this._taskbarIcon = (this.MainWindow!.Content as TaskbarIcon);
+            this._taskbarIcon!.TrayBalloonTipClicked += this.OnBalloonClick;
+            this._taskbarIcon.TrayMouseDoubleClick += this.OnTaskbarIconDoubleClick;
+
+            var runAtStartUpMenuItem = this._taskbarIcon.ContextMenu.Items.GetItemAt(1) as MenuItem;
+            runAtStartUpMenuItem.IsChecked = this.WillRunOnStartUp();
+
+            this._timer = new Timer(async _ => {
+                try {
+                    await CheckAzure();
+                } catch (Exception e) {
+                    Log.Error(e, "Failed to refresh with Azure");
+                    this.Dispatcher.Invoke(() => {
+                        this._taskbarIcon.ToolTipText = $"Azure PRMate (error)";
+                    });
+                }
+            }, null, 0, this._fetchFrequency * 1_000);
+        }
+
+        private void OnContextMenuOpened(object sender, RoutedEventArgs e) {
+            var runAtStartUpMenuItem = this._taskbarIcon.ContextMenu.Items.GetItemAt(1) as MenuItem;
+            runAtStartUpMenuItem.IsChecked = this.WillRunOnStartUp();
+        }
+
+        private void OnStartUpMenuItemClick(object sender, RoutedEventArgs e) {
+            var key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (this.WillRunOnStartUp()) {
+                key.DeleteValue("AzurePRMate");
+            } else {
+                key.SetValue("AzurePRMate", Process.GetCurrentProcess().MainModule.FileName);
+            }
+        }
+
+        private void OnOpenAzureDevOpsMenuItemClick(object sender, RoutedEventArgs e) {
+            this.OpenBrowser("https://dev.azure.com/PebblePad/_pulls");
+        }
+
+        private void OnExitMenuItemClick(object sender, RoutedEventArgs e) {
+            this.Dispose();
+            Environment.Exit(0);
+        }
+
+        private void OnBalloonClick(object sender, RoutedEventArgs e) {
+            this.OpenBrowser("https://dev.azure.com/PebblePad/_pulls");
+        }
+
+        private void OnTaskbarIconDoubleClick(object sender, RoutedEventArgs e) {
+            this.OpenBrowser("https://dev.azure.com/PebblePad/_pulls");
+        }
+
+        private async Task CheckAzure() {
+            this.Dispatcher.Invoke(() => {
+                this._taskbarIcon.ToolTipText += $" (fetching...)";
+            });
+
+            await this._azurePrService.RunAutomationsAsync();
+            var count = await this._azurePrService.GetPullRequestCountAsync();
+
+            this.Dispatcher.Invoke(() => {
+                this._taskbarIcon.IconSource = BitmapFrame.Create(new Uri($"pack://application:,,,/AzurePRMate;component/Icons/devops{(count > 0 ? "-new" : "")}.ico"));
+                this._taskbarIcon.ToolTipText = $"Azure PRMate ({count} awaiting)";
+                if (count > 0 && this._lastReminderTime < DateTime.UtcNow.AddMinutes(-this._reminderFrequency)) {
+                    this._taskbarIcon.ShowBalloonTip("Pull Requests waiting", $"You have {count} pull request(s) awaiting your attention. Click the Azure icon to view them.", BalloonIcon.Info);
+                    this._lastReminderTime = DateTime.UtcNow;
+                }
+            });
+        }
+
+        private void OpenBrowser(string url) {
+            try {
+                Process.Start(url);
+            } catch {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+                    Process.Start("xdg-open", url);
+                } else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+                    Process.Start("open", url);
+                } else {
+                    throw;
+                }
+            }
+        }
+
+        private bool WillRunOnStartUp() {
+            var key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            var value = (string) key.GetValue("AzurePRMate", null);
+            return !string.IsNullOrWhiteSpace(value);
+        }
+
+
+    }
+}
---
+++ AzurePrService.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace AzurePRMate;
+
+class AzurePrService(IConfiguration config): IDisposable {
+        
+    private const string URI = "https://dev.azure.com/PebblePad";
+        
+    private readonly bool _includeWaitingForAuthor = config.GetValue("IncludeWaitingForAuthor", true);
+    private readonly bool _includeRejected = config.GetValue("IncludeRejected", false);
+    private readonly bool _includeDraft = config.GetValue("IncludeDraft", false);
+    private readonly WaitingForAuthorClearConfiguration _clearWaitingForAuthor =
+        config.GetValue("ClearWaitingForAuthorVotes", WaitingForAuthorClearConfiguration.Never);
+    private readonly VssConnection _connection = 
+        new (new Uri(URI), new VssBasicCredential("", config.GetValue<string>("Azure:PersonalAccessToken")));
+
+    public async Task<int> GetPullRequestCountAsync() {
+        var pullRequests = await this.GetUsersPullRequestsAsync();
+
+        return pullRequests.Count(pr => {
+            if (!this._includeDraft && pr.IsDraft == true) {
+                return false;
+            }
+            var review = pr.Reviewers.First(r => r.Id == this._connection.AuthorizedIdentity.Id.ToString());
+            return !review.HasDeclined.GetValueOrDefault(false)
+                   && (review.Vote == 0
+                       || (this._includeWaitingForAuthor && review.Vote == -5)
+                       || (this._includeRejected && review.Vote == -10));
+        });
+    }
+
+    public async Task RunAutomationsAsync() {
+        var gitClient = this.GetGitClient();
+        await ClearWaitingForAuthorVotes(gitClient);
+    }
+
+    private async Task ClearWaitingForAuthorVotes(GitHttpClient gitClient) {
+        if (this._clearWaitingForAuthor == WaitingForAuthorClearConfiguration.Never) {
+            return;
+        }
+        var pullRequests = await this.GetUsersPullRequestsAsync();
+        foreach (var pr in pullRequests) {
+            IdentityRefWithVote review;
+            try
+            {
+                review = pr.Reviewers.First(r => r.Id == this._connection.AuthorizedIdentity.Id.ToString());
+            }
+            catch (Exception e)
+            {
+                Log.Warning(e, "Could not get threads for Pull Request \"{Pull Request Title}\".", pr.Title);
+                continue;
+            }
+
+            if (review.Vote == -5 /* Waiting for Author */) {
+                var clear = false;
+                var threads = await gitClient.GetThreadsAsync(pr.Repository.Id, pr.PullRequestId);
+                if (this._clearWaitingForAuthor == WaitingForAuthorClearConfiguration.AllCommentsResolved) {
+                    var allThreadsResolved = threads.All(thread => thread.IsDeleted
+                                                                   || !(thread.Status == CommentThreadStatus.Active || thread.Status == CommentThreadStatus.Pending));
+                    clear = allThreadsResolved;
+                } else if (this._clearWaitingForAuthor == WaitingForAuthorClearConfiguration.PullRequestUpdated) {
+                    var iterations = await gitClient.GetPullRequestIterationsAsync(pr.Repository.Id, pr.PullRequestId);
+                    var prLastUpdated = iterations.OrderByDescending(iteration => iteration.UpdatedDate).FirstOrDefault();
+                    var lastWaitingForAuthorThread = threads.OrderByDescending(thread => thread.PublishedDate).FirstOrDefault(thread => {
+                        var comment = thread.Comments.First();
+                        return comment.CommentType == CommentType.System
+                               && comment.Content == $"{review.DisplayName} voted -5";
+                    });
+                    clear = prLastUpdated.UpdatedDate > lastWaitingForAuthorThread.PublishedDate;
+                }
+                if (clear) {
+                    var newReview = new IdentityRefWithVote() { Id = review.Id, Vote = 0 };
+                    try
+                    {
+                        await gitClient.UpdatePullRequestReviewersAsync(new[] { newReview }, pr.Repository.Id, pr.PullRequestId);
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Warning(e, "Could not update review on Pull Request \"{Pull Request Title}\".", pr.Title);
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    public async Task<List<GitPullRequest>> GetUsersPullRequestsAsync() {
+        var searchCriteria = new GitPullRequestSearchCriteria() {
+            Status = PullRequestStatus.Active,
+            ReviewerId = this._connection.AuthorizedIdentity.Id,
+        };
+
+        return await GetPullRequests(searchCriteria);
+    }
+
+    public async Task<List<GitPullRequest>> GetPullRequests(GitPullRequestSearchCriteria searchCriteria) {
+        var pullRequests = new List<GitPullRequest>();
+
+        var client = this.GetGitClient();
+
+        var repositories = await client.GetRepositoriesAsync();
+        foreach (var repository in repositories) {
+            var pullRequestsForRepo = new List<GitPullRequest>();
+            try
+            {
+                pullRequestsForRepo = await client.GetPullRequestsAsync(repository.Id.ToString(), searchCriteria);
+            } catch (VssServiceException e) {
+                Log.Warning(e, "Could not get pull requests from repository \"{Repository Name}\".", repository.Name);
+            }
+            pullRequests.AddRange(pullRequestsForRepo);
+        }
+
+        return pullRequests;
+    }
+        
+    private GitHttpClient GetGitClient() {
+        return this._connection.GetClient<GitHttpClient>();
+    }
+
+    public void Dispose() {
+        this._connection.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
+    
+enum WaitingForAuthorClearConfiguration {
+    Never,
+    PullRequestUpdated,
+    AllCommentsResolved
+}
